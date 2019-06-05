bddcbac5aee68531b5b7def266b7b54c0267658a
Pasha <matrunarto@desoz.com>
2019-06-05 21:18:39 +0300

    Add functionality

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+const controlsPanel = document.getElementsByClassName('controls')[0]
+const counterElement = document.getElementsByClassName('counter')[0]
 
+let counter = 0;
+
+
+function updateCouner(newValue) {
+  counterElement.innerHTML = newValue
+}
+
+updateCouner(counter)
+
+document.addEventListener('click', (event) => {
+
+    if(event.target.closest('.plus')) {
+       counter++
+    } 
+
+    if(event.target.closest('.reset')) {
+       counter = 0
+    } 
+
+    if(event.target.closest('.minus')) {
+       counter--
+    } 
+
+    updateCouner(counter)
+})
